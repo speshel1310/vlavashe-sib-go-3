5788eea2a0e97597c0487a8b61951ee39777bdfb
speshel1310 <speshel1310@gmail.com>
2025-11-27 15:06:43 +0300

    обновил номер аналитики

--- game.js
+++ game.js
@@ -1281,7 +1281,7 @@ class Game {
         try {
             if (typeof ym !== 'undefined') {
                 console.log(`Отправка события: ${eventName}`, params);
-                ym(105435485, 'reachGoal', eventName, params);
+                ym(105524141, 'reachGoal', eventName, params);
             }
         } catch (error) {
             console.error('Ошибка при отправке события в метрику:', error);
